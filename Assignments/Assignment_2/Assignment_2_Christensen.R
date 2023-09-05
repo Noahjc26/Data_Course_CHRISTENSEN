74d5c8fd88dfac59c09cf1eff191635977cb5740
Noahjc26 <10851943@uvu.edu>
2023-09-05 11:54:09 -0600

    Finished Assignment 2

--- Assignments/Assignment_2/Assignment_2_Christensen.R
+++ Assignments/Assignment_2/Assignment_2_Christensen.R
@@ -5,25 +5,32 @@
 # the current directory "."
 #The directory above the current one ".."
 
-
-list.files(.)
 list.files(path="Data")
-csv_files <- list.files(path="Data",pattern = ".csv") 
-# option minus for <- 
+
+csv_files <- list.files(path="Data",
+                        full.names = TRUE,
+                        pattern = ".csv") # option minus for <- 
 length(csv_files)
 
-?length # the question mark goes to the 'Help' tab
+?length     # the question mark goes to the 'Help' tab
 
-df <- read.csv("./Data/wingspan_vs_mass.csv") 
-# df = data frame
+df <- read.csv("./Data/wingspan_vs_mass.csv")  # df = data frame
 
-head(df,n=5)  #head shows first 6 lines, n specifies the number of lines)
-list.files(path="Data",
+head(df,n=5)    #head shows first 6 lines, n specifies the number of lines)
+
+bfiles <- list.files(path="Data",
            recursive = TRUE,
+           full.names=TRUE,
            pattern = "^b")
 
+bfiles
 
-list.files(path="Data",
-           recursive = TRUE,
-           pattern = ".csv")
+for(i in bfiles) {
+  print(readLines(i,n=1))
+} #i is just "iteration" 
+  
+for(i in csv_files) {
+  print(readLines(i,n=1))
+}
 
+#"Command-Option-B" runs all lines above
