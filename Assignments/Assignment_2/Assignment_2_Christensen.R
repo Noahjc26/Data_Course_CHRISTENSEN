362b7a281e378ca3e540959007e76b76c5bec2b5
Noahjc26 <10851943@uvu.edu>
2023-09-05 09:07:08 -0600

    update Assignment 2

--- Assignments/Assignment_2/Assignment_2_Christensen.R
+++ Assignments/Assignment_2/Assignment_2_Christensen.R
@@ -1,17 +1,29 @@
 
 # Assignment 2
 
-#notes absolute vs relative ./..
+#root "/"
+# the current directory "."
+#The directory above the current one ".."
+
+
 list.files(.)
 list.files(path="Data")
 csv_files <- list.files(path="Data",pattern = ".csv") 
 # option minus for <- 
 length(csv_files)
-?length
+
+?length # the question mark goes to the 'Help' tab
 
 df <- read.csv("./Data/wingspan_vs_mass.csv") 
 # df = data frame
-head(df,n=5)
+
+head(df,n=5)  #head shows first 6 lines, n specifies the number of lines)
 list.files(path="Data",
            recursive = TRUE,
            pattern = "^b")
+
+
+list.files(path="Data",
+           recursive = TRUE,
+           pattern = ".csv")
+
