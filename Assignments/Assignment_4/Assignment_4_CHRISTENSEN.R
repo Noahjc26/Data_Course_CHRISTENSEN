f48122a466cdeddf7d851885f4671b72d749290e
Noahjc26 <10851943@uvu.edu>
2023-09-17 16:58:19 -0600

    final upload hopefully

--- Assignments/Assignment_4/Assignment_4_CHRISTENSEN.R
+++ Assignments/Assignment_4/Assignment_4_CHRISTENSEN.R
@@ -1,5 +1,8 @@
 library(tidyverse)
-df <- read.csv("grngrass.csv")
+list.files(path="Assignments/Assignment_4")
+
+df <- read.csv("Assignments/Assignment_4/grngrass.csv")
+#path is based on Data_Course_CHRISTENSEN as directory
 
 ggplot(df,aes(x=wavelength,y=reflectance)) +
   geom_line()
